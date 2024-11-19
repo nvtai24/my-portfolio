672eb2a281c0f1066ca5ea4d306997a4eae8c0b8
Nvtai <buiblue123@gmail.com>
2024-11-19 15:17:22 +0700

    log to check

--- frontend/assets/js/email-handler.js
+++ frontend/assets/js/email-handler.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contact-form");
 
+  console.log("EMAILJS_USER_ID:", process.env.EMAILJS_USER_ID);
+  console.log("EMAILJS_SERVICE_ID:", process.env.EMAILJS_SERVICE_ID);
+  console.log("EMAILJS_TEMPLATE_ID:", process.env.EMAILJS_TEMPLATE_ID);
+  console.log("EMAILJS_PRIVATE_KEY:", process.env.EMAILJS_PRIVATE_KEY);
+
   form.addEventListener("submit", function (e) {
     e.preventDefault();
 
