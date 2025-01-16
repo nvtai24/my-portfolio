757d80c466ffa79851fc8f4cfc730925b9f39973
Nvtai <buiblue123@gmail.com>
2025-01-16 17:34:18 +0700

    fix cors - change domain name

--- backend/api/send-email.js
+++ backend/api/send-email.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
   // Cấu hình CORS headers
-  const allowedOrigin = "https://nvtai24-portfolio.vercel.app";
+  const allowedOrigin = "https://nvtai24.vercel.app";
   res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -21,18 +21,24 @@ module.exports = async (req, res) => {
     console.log("Received data from frontend:", req.body);
 
     try {
-      const response = await axios.post("https://api.emailjs.com/api/v1.0/email/send", {
-        service_id: process.env.EMAILJS_SERVICE_ID,
-        template_id: process.env.EMAILJS_TEMPLATE_ID,
-        user_id: process.env.EMAILJS_USER_ID,
-        accessToken: process.env.EMAILJS_PRIVATE_KEY,
-        template_params: { to_name, from_name, from_email, message },
-      });
+      const response = await axios.post(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        {
+          service_id: process.env.EMAILJS_SERVICE_ID,
+          template_id: process.env.EMAILJS_TEMPLATE_ID,
+          user_id: process.env.EMAILJS_USER_ID,
+          accessToken: process.env.EMAILJS_PRIVATE_KEY,
+          template_params: { to_name, from_name, from_email, message },
+        }
+      );
 
       console.log("Email sent successfully:", response.data);
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (error) {
-      console.error("Error sending email:", error.response?.data || error.message);
+      console.error(
+        "Error sending email:",
+        error.response?.data || error.message
+      );
       res.status(500).json({ message: "Failed to send email." });
     }
   } else {
